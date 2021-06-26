5831a7928a02e4920d3f193ace6a787571a49101
imabhisht <abhishtchouhan@gmail.com>
2021-06-26 23:13:42 +0530

    Update Post Fixed

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,10 @@ import { getPosts } from "./actions/posts";
 const App = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
-
+  const [currentId, setCurrentId] = React.useState(null);
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
+  }, [currentId, dispatch]);
   return (
     <Container maxidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
@@ -35,11 +35,11 @@ const App = () => {
             spacing={3}
           >
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
 
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,9 +2,9 @@ import React from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
-import { useDispatch } from "react-redux";
-import { createPost } from "../../actions/posts";
-const Form = () => {
+import { useDispatch, useSelector } from "react-redux";
+import { createPost, updatePost } from "../../actions/posts";
+const Form = ({ currentId, setCurrentId }) => {
   const classes = useStyles();
   const [postData, setPostData] = React.useState({
     creator: "",
@@ -13,14 +13,36 @@ const Form = () => {
     tags: "",
     selectedFile: "",
   });
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((p) => p._id === currentId) : null
+  );
   const dispatch = useDispatch();
+  React.useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(createPost(postData));
+
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
+      clear();
+    } else {
+      dispatch(createPost(postData));
+      clear();
+    }
   };
 
-  const clear = () => {};
+  const clear = () => {
+    setCurrentId(null);
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
 
   return (
     <Paper className={classes.paper}>
@@ -30,7 +52,9 @@ const Form = () => {
         className={`${classes.form},${classes.root}`}
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">Creating Memories</Typography>
+        <Typography variant="h6">
+          {currentId ? "Editing" : "Create"} a Memory
+        </Typography>
 
         <TextField
           name="creator"
